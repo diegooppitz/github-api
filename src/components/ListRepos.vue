4768d891e16a46481d86d617d2acb470a1ce79f6
Diego Oppitz <diego.rodrigues@pmweb.com.br>
2020-03-06 16:43:35 -0300

    Add label colors for technos

--- src/components/ListRepos.vue
+++ src/components/ListRepos.vue
@@ -1,26 +1,17 @@
 <template>
   <div class="listRepos">
-    <button class="reposButton" v-if="!starRep" @click="updateRepos()">
-      Ver repositórios favoritos
-    </button>
-    <button class="reposButton" v-if="starRep" @click="updateRepos()">
-      Ver repositórios públicos
-    </button>
+    <button class="reposButton" v-if="!starRep" @click="updateRepos()">Ver repositórios favoritos</button>
+    <button class="reposButton" v-if="starRep" @click="updateRepos()">Ver repositórios públicos</button>
 
     <div class="repos" v-if="!starRep">
       <h3 class="reposTitle">Repositórios públicos:</h3>
       <template class="currency" v-for="repos in repReqV.data">
         <div :key="repos.name">
-          <a
-            target="_blank"
-            :href="`https://github.com/${userN + '/' + repos.name}`"
-          >
+          <a target="_blank" :href="`https://github.com/${userN + '/' + repos.name}`">
             <h4>{{ repos.name }}</h4>
           </a>
-          <p class="technologies">{{ repos.language }}</p>
-          <p class="date">
-            {{ repos.created_at | moment("calendar", "July 10 2011") }}
-          </p>
+          <p class="technologies" :class="verifTechno(repos.language)">{{ repos.language }}</p>
+          <p class="date">{{ repos.created_at | moment("calendar", "July 10 2011") }}</p>
           <br />
           <p class="description">{{ repos.description }}</p>
         </div>
@@ -34,12 +25,11 @@
           <a :href="`https://github.com/${userN + '/' + starred.name}`">
             <h4>{{ starred.name }}</h4>
           </a>
-          <p class="technologies">
-            {{ starred.language }}
-          </p>
-          <p class="date">
-            {{ starred.created_at | moment("calendar", "July 10 2011") }}
-          </p>
+          <p
+            class="technologies"
+            :class="verifTechno(starred.language)"
+          >{{ starred.language }}</p>
+          <p class="date">{{ starred.created_at | moment("calendar", "July 10 2011") }}</p>
           <br />
           <p class="description">{{ starred.description }}</p>
         </div>
@@ -87,9 +77,43 @@ export default {
     updateRepos() {
       this.starRep = !this.starRep;
     },
-    verifTechno() {
-      if( this.starred.name == "javaScript")
-      this.technoColor = "red";
+    verifTechno(value) {
+      if (value && value.length > 1) {
+        switch (value) {
+          case "JavaScript":
+            return "burntYellow";
+            break;
+          case "Java":
+            return "dimGrey";
+            break;
+          case "PHP":
+            return "phpColor";
+            break;
+          case "HTML":
+            return "htmlColor";
+            break;
+          case "Python":
+            return "green";
+            break;
+          case "CSS":
+            return "red";
+            break;
+          case "Shell":
+            return "blue";
+            break;
+          case "Go":
+            return "yellow";
+            break;
+          case "Vue":
+            return "vueColor";
+            break;
+          default:
+            return "black";
+            break;
+        }
+      } else {
+        return "notTechno";
+      }
     }
   }
 };
@@ -123,9 +147,9 @@ p {
   }
 }
 
-.repos p {
-  font-size: 14px;
-}
+// .repos p {
+//   font-size: 14px;
+// }
 
 .reposButton {
   font-weight: 900;
@@ -148,11 +172,72 @@ p {
   margin: 5px 0 20px 0;
 }
 
-.technologies {
-  margin: 0 10px 0 0;
-}
-
 .date {
   margin: 0;
+}
+
+.technologies {
+  margin: 0 10px 0 0;
+  padding: 3px 12px;
+  font-size: 10px;
+  font-weight: 900;
+  color: #ffffff;
+}
+
+.blue {
+  background-color: #0000FF;
+}
+
+.burntYellow {
+  background-color: #e1ad01;
+}
+
+// Labels Colors
+.red {
+  background-color: #dc143c;
+}
+
+.black {
+  background-color: #000;
+}
+
+.yellow {
+  background-color: #ffff00;
+}
+
+.dimGrey {
+  background-color: #696969;
+}
+
+.green {
+  background-color: green;
+}
+
+.orange {
+  background-color: #ff8c00;
+}
+
+.htmlColor {
+  background-color: #e75f35;
+}
+
+.phpColor {
+  background-color: #5283A3;
+}
+
+.typeScriptColor {
+  background-color: #715CC1;
+}
+
+.vueColor {
+  background-color: #42b884;
+}
+
+.notTechno {
+  margin: initial;
+  padding: initial;
+  font-size: initial;
+  font-weight: initial;
+  color: initial;
 }
 </style>
